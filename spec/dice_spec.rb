6851d6bce2c5ad5164397c0efc6e285ce26ec9b2
Alex Hutchings <alexhutchings4@gmail.com>
2019-01-10 11:33:06 +0000

    Added unit test for dice.roll to produce a random number between 1 and 6

---
+++ spec/dice_spec.rb
@@ -0,0 +1,17 @@
+require 'dice'
+
+describe Dice do
+
+  describe '#roll' do
+
+    it { is_expected.to respond_to(:roll) }
+
+    it 'should return a random number between 1 and 6' do
+      srand(6)
+      dice = Dice.new
+      expect(dice.roll).to eq 3
+    end
+
+  end
+
+end
