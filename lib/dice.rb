5335652c84fd6193e3eac92c1ab008210ac07c65
Alex Hutchings <alexhutchings4@gmail.com>
2019-01-10 12:37:18 +0000

    Dice program is now able to take multiple dice and return the total, plus the die rolled. All tests passed

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,20 @@
 class Dice
-  def roll(a_number_of_dice)
-    rand(1..6)
+
+  def initialize
+    @total = []
+  end
+
+  def roll(n)
+    @total = []
+    n.times { score }
+    return "You rolled a total of #{@total.sum} #{@total.to_s}" if n >= 2
+    return "You rolled a #{@total.sum}" if n == 1
+    "You rolled nothing..."
+  end
+
+  private
+
+  def score
+    @total << rand(1..6)
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,13 +3,23 @@ require 'dice'
 describe Dice do
 
   describe '#roll' do
+    before(:each) do
+      srand(6)
+      @dice = Dice.new
+    end
 
     it { is_expected.to respond_to(:roll).with(1).argument }
 
     it 'should return a random number between 1 and 6' do
-      srand(6)
-      dice = Dice.new
-      expect(dice.roll(1)).to eq 3
+      expect(@dice.roll(1)).to eq "You rolled a 3"
+    end
+
+    it 'should return 2 random numbers between 1 and 6 and the total' do
+      expect(@dice.roll(2)).to eq "You rolled a total of 5 [3, 2]"
+    end
+
+    it 'should return 3 random numbers between 1 and 6 and the total' do
+      expect(@dice.roll(3)).to eq "You rolled a total of 9 [3, 2, 4]"
     end
 
   end
